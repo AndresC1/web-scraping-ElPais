cca19e67246b9cf6b8e8aeb1f563758418a6f998
AndresC1 <andrescornejolira@gmail.com>
2023-11-09 18:41:00 -0600

    fix:(news): Ajustes en el svg

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -8,9 +8,12 @@
                 <span class="col-span-8 flex flex-col justify-start items-start gap-4 px-4">
                     <h1 class="font-semibold text-lg">{{$new['title']}}</h1>
                     <p class="text-[#777] text-sm font-medium">{{$new['content']}}</p>
-                    <a class="col-span-12 flex justify-end items-center gap-2" href="{{route('info', ['country' => $new['link']['country'], 'date' => $new['link']['date'], 'title' => $new['link']['title']])}}">
+                    <a class="col-span-12 flex justify-end items-center gap-2"
+                       href="{{route('info', ['country' => $new['link']['country'], 'date' => $new['link']['date'], 'title' => $new['link']['title']])}}">
                         <span class="text-sm font-medium text-[#777]">Leer más</span>
-                        <svg class="w-4 h-4 text-[#777]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17 8l4 4m0 0l-4 4m4-4H3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                        <svg class="w-4 h-4 text-[#777]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17 8l4 4m0 0l-4 4m4-4H3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
                     </a>
                 </span>
             </div>
